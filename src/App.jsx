dc54503a77a649a8e93db4f1b10a88455fe979a4
Solomon <soluwaseun806@gmail.com>
2024-06-08 09:35:45 +0100

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <div className="min-h-screen  bg-black text-white flex justify-center items-center">
      <Sidebar/>
+     <button className='fixed top-2 right-4 hover:cursor-pointer md:hidden'><img src="src/assets/hamburgerMenu.svg" alt="" /></button>
       <div className="flex-1  ml-0 md:ml-64">
         <MusicPlayer />
       </div>
--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -10,20 +10,23 @@ const Upload = ({ onUpload }) => {
   };
 
   return (
-  <div className="min-h-screen flex flex-col  ">
+  <div className="min-h-screen mt-12 md:mt-0 flex flex-col  ">
     <div className=" p-6">
-      <p className='font-semibold'>Add Music</p>
+      <p className='font-semibold text-3xl'>Add Music</p>
     </div>
   <div className="md:flex-1 flex justify-center items-center">
     <div className="flex flex-col bg-uploadBackground rounded-md p-8">
       <input
         type="file"
         onChange={handleFileUpload}
-        className="mt-1 block w-full text-sm text-white bg-gray-900/30 p-6 rounded-lg  cursor-pointer focus:outline-none"
+        className="mt-1  w-full text-sm text-white bg-gray-900/30 p-6 rounded-lg  cursor-pointer focus:outline-none"
+        id='fileInput'
       />
       <div className='flex mt-4 justify-between items-center'>
-        <label className="border border-customPurple px-5 py-2 text-sm font-medium  ">Upload Music</label>
-        <button className='bg-customPurple px-4 py-1 rounded-lg '>Add</button>
+        <button className="border border-customPurple px-5 py-2 text-sm font-medium  hover:cursor-pointer">Upload Music</button>
+        <button 
+        onClick={() => document.getElementById('fileInput').click()}
+        className='bg-customPurple px-8 py-1 rounded-lg hover:cursor-pointer'>Add</button>
       </div>
     </div>
   </div>
